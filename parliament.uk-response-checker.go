1e06b99ce79d440b64655a5168694cc4be522cf9
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-11-30 15:22:33 +0000

    Reduced the number of files, split out into separate methods

--- parliament.uk-response-checker.go
+++ parliament.uk-response-checker.go
@@ -7,12 +7,12 @@ import (
   // "encoding/json"
   "fmt"
   "io"
-  // "io/ioutil"
+  "io/ioutil"
   "log"
-  // "net/http"
+  "net/http"
   "os"
-  // "regexp"
-  // "strings"
+  "regexp"
+  "strings"
   // "unicode/utf8"
 )
 
@@ -30,95 +30,106 @@ type Link struct {
 }
 
 func main(){
-  // 1. Create a new file, result.txt (if it doesn't already exist)
-  // _, err := os.Create("results.txt")
-  // checkError(err)
-
   // 2. Get http response with links
-  // linksResponse, err := http.Get("https://raw.githubusercontent.com/ukparliament/ontologies/master/urls.csv")
-  // checkError(err)
-  // defer linksResponse.Body.Close()
+  linksResponse, err := http.Get("https://raw.githubusercontent.com/ukparliament/ontologies/master/urls.csv")
+  checkError(err)
+  defer linksResponse.Body.Close()
 
-  // body, err := ioutil.ReadAll(linksResponse.Body)
-  // bodyString := string(body)
+  body, err := ioutil.ReadAll(linksResponse.Body)
+  bodyString := string(body)
 
   // // 3. Create a new file, output.csv (if it doesn't already exist) to write results to
-  // outputFile, err := os.Create("output.txt")
-  // checkError(err)
-  // defer outputFile.Close()
+  outputFile, err := os.Create("output.txt")
+  checkError(err)
+  defer outputFile.Close()
 
   // Replace carriage return with new line
-  // var r = regexp.MustCompile("\r")
-  // s := r.ReplaceAllString(bodyString, "\n")
+  var r = regexp.MustCompile("\r")
+  s := r.ReplaceAllString(bodyString, "\n")
 
   // Write response to outputFile
-  // writer := bufio.NewWriter(outputFile)
-  // fmt.Fprintf(writer, "%v", s)
+  writer := bufio.NewWriter(outputFile)
+  fmt.Fprintf(writer, "%v", s)
 
+  parseLinks()
+}
 
-  // Create new file for lines
-  linesFile, err := os.Create("somelines.txt")
+func parseLinks() {
+  // Create a new file, result.txt (if it doesn't already exist)
+  resultFile, err := os.Create("results.txt")
   checkError(err)
+  defer resultFile.Close()
 
-  // 4. Open output file and parse each comma separated value
+  // Open output file and parse each comma separated value
   outputFile, err := os.Open("output.txt")
   checkError(err)
   defer outputFile.Close()
 
   csvReader := csv.NewReader(outputFile)
 
-  // Create an array containing 4 comma separated values
   value := ""
 
   for {
     separatedValues, err := csvReader.Read()
     checkError(err)
+
+    // Logic to separate out each link
+    for i, word := range separatedValues {
+      if i % 4 == 0 && string(word) != "" && string(separatedValues[i+1]) != "Route" {
+        value = replaceResourceId(separatedValues[i + 1])
+        // Make call to link and record response code
+        recordLinkStatus(value, resultFile)
+        value = ""
+      }
+    }
     if err == io.EOF {
       break
-    }
-
-    length := len(separatedValues)
-    writer := bufio.NewWriter(linesFile)
-
-    for i, word := range separatedValues {
-      fmt.Printf("number: %v Word: %v\n", i, word)
-      if i % 4 == 0 && string(word) != "" {
-        value = separatedValues[i + 1]
-        fmt.Fprintf(writer, "%v\n", value)
-        value = ""
-        writer.Flush()
-      } else if (i + 1) == length {
-        break
-      }
     }
   }
 }
 
 
-resourceIdMap := map[string]int{
-  ":source": "mnisId",
-  ":id": "3299",
-  ":letters": "g",
-  ":person": "TyNGhslR",
-  ":contact-point",
-  ":constituency": "3WLS0fFd",
-  ":party": "DIifZMjq",
-  ":house": "1AFu55Hs",
-  ":parliament": "b0t56VVL",
-  ":postcode": "SW1A 0AA",
-  ":medium": "3UJ7otWM",
-  ":resource": "S70cUJGM",
+func recordLinkStatus(url string, resultFile *os.File){
+  // Create new Link object
+  link := Link{url: url}
+
+  // Visit link
+  response, err := http.Get(baseUrl + link.url)
+  checkError(err)
+  defer response.Body.Close()
+
+  // Get response code
+  link.code = response.StatusCode
+
+  // Write Response to file
+  writer := bufio.NewWriter(resultFile)
+  fmt.Fprintf(writer, "%v, %v\n", link.url, link.code)
+
+  writer.Flush()
 }
 
-func replaceResourceId(word string) {
-  if
-}
+func replaceResourceId(word string) string {
+  resourceIdMap := map[string]string {
+    ":source": "mnisId",
+    ":id": "3299",
+    ":letters": "g",
+    ":person": "TyNGhslR",
+    ":contact-point": "wk1atnfh",
+    ":constituency": "3WLS0fFd",
+    ":party": "DIifZMjq",
+    ":house": "1AFu55Hs",
+    ":parliament": "b0t56VVL",
+    ":place": "E15000006",
+    ":postcode": "SW1A 0AA",
+    ":medium": "3UJ7otWM",
+    ":resource": "S70cUJGM",
+  }
 
-// Code for writing request and response headers and body
-// client := &http.Client{}
-//
-// request, err := http.NewRequest("GET", "https://raw.githubusercontent.com/ukparliament/ontologies/master/urls.csv", nil)
-// response, err := client.Do(request)
-//
-// request.Write(outputFile)
-// response.Write(outputFile)
+  // Replace with valid ids
+  for id, value := range resourceIdMap {
+    if strings.Contains(word, id) {
+      word = strings.Replace(word, id, value, -1)
+    }
+  }
+  return word
+}
